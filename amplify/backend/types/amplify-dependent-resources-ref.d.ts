b3454bdadf9c86fe7fb4d6cb8b9b4528b5a205df
frank gimeno <frankgimeno3@gmail.com>
2024-03-20 20:35:05 +0100

    auth aws funciona perfecto, signout tb

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyfullstack": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyfullstack73ffef4e": {
       "AppClientID": "string",
